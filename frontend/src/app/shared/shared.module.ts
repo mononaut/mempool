e84fac1b7522b6beb4fb8f31f2da6ca8d6073c1d
Mononaut <github@monospace.live>
2022-07-12 22:04:20 +0000

    Add open graph block preview page

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -7,6 +7,7 @@ import { faFilter, faAngleDown, faAngleUp, faAngleRight, faAngleLeft, faBolt, fa
   faFileAlt, faRedoAlt, faArrowAltCircleRight, faExternalLinkAlt, faBook, faListUl, faDownload } from '@fortawesome/free-solid-svg-icons';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { MasterPageComponent } from '../components/master-page/master-page.component';
+import { MasterPagePreviewComponent } from '../components/master-page-preview/master-page-preview.component';
 import { BisqMasterPageComponent } from '../components/bisq-master-page/bisq-master-page.component';
 import { LiquidMasterPageComponent } from '../components/liquid-master-page/liquid-master-page.component';
 import { AboutComponent } from '../components/about/about.component';
@@ -46,6 +47,7 @@ import { TransactionComponent } from '../components/transaction/transaction.comp
 import { TransactionsListComponent } from '../components/transactions-list/transactions-list.component';
 import { BlockComponent } from '../components/block/block.component';
 import { BlockAuditComponent } from '../components/block-audit/block-audit.component';
+import { BlockPreviewComponent } from '../components/block/block-preview.component';
 import { BlockOverviewGraphComponent } from '../components/block-overview-graph/block-overview-graph.component';
 import { BlockOverviewTooltipComponent } from '../components/block-overview-tooltip/block-overview-tooltip.component';
 import { AddressComponent } from '../components/address/address.component';
@@ -108,12 +110,14 @@ import { SvgImagesComponent } from '../components/svg-images/svg-images.componen
 
     AboutComponent,
     MasterPageComponent,
+    MasterPagePreviewComponent,
     BisqMasterPageComponent,
     LiquidMasterPageComponent,
     StartComponent,
     TransactionComponent,
     BlockComponent,
     BlockAuditComponent,
+    BlockPreviewComponent,
     BlockOverviewGraphComponent,
     BlockOverviewTooltipComponent,
     TransactionsListComponent,
@@ -210,6 +214,7 @@ import { SvgImagesComponent } from '../components/svg-images/svg-images.componen
     TransactionComponent,
     BlockComponent,
     BlockAuditComponent,
+    BlockPreviewComponent,
     BlockOverviewGraphComponent,
     BlockOverviewTooltipComponent,
     TransactionsListComponent,
