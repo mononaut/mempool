9203fc45b91ed963359e7d74850883baa1b21929
Mononaut <github@monospace.live>
2022-10-19 20:09:13 +0000

    audit debug logging

--- backend/src/api/audit.ts
+++ backend/src/api/audit.ts
@@ -1,3 +1,4 @@
+import logger from '../logger';
 import { BlockExtended, TransactionExtended, MempoolBlockWithTransactions } from '../mempool.interfaces';
 
 const PROPAGATION_MARGIN = 180; // in seconds, time since a transaction is first seen after which it is assumed to have propagated to all miners
@@ -12,6 +13,8 @@ class Audit {
     const isCensored = {}; // missing, without excuse
     const isDisplaced = {};
     let displacedWeight = 0;
+
+    logger.warn(`projected block with ${projectedBlocks[0].transactionIds.length} txs weight ${projectedBlocks[0].blockVSize * 4}`);
 
     const inBlock = {};
     const inTemplate = {};
@@ -48,12 +51,14 @@ class Audit {
     let index = 0;
     let lastFeeRate = Infinity;
     let failures = 0;
+    let totalDisplacedWeight = 0;
     while (projectedBlocks[1] && index < projectedBlocks[1].transactionIds.length && failures < 500) {
       const txid = projectedBlocks[1].transactionIds[index];
       const fits = (mempool[txid].weight - displacedWeightRemaining) < 4000;
       const feeMatches = mempool[txid].effectiveFeePerVsize >= lastFeeRate;
       if (fits || feeMatches) {
         isDisplaced[txid] = true;
+        totalDisplacedWeight += mempool[txid].weight;
         if (fits) {
           lastFeeRate = Math.min(lastFeeRate, mempool[txid].effectiveFeePerVsize);
         }
@@ -66,6 +71,8 @@ class Audit {
       }
       index++;
     }
+
+    logger.warn(`identified ${Object.keys(isDisplaced).length} displaced txs, weight ${totalDisplacedWeight}`);
 
     // mark unexpected transactions in the mined block as 'added'
     let overflowWeight = 0;
@@ -80,9 +87,13 @@ class Audit {
       }
     }
 
+    logger.warn(`mined block with ${matches.length} matches ${added.length} added, ${transactions.length - matches.length - added.length} selected, ${overflowWeight} overflow weight`);
+
     // transactions missing from near the end of our template are probably not being censored
     let overflowWeightRemaining = overflowWeight;
     let lastOverflowRate = 1.00;
+    let sameFeeRate = 0;
+    let sameFeeRateWeight = 0;
     index = projectedBlocks[0].transactionIds.length - 1;
     while (index >= 0) {
       const txid = projectedBlocks[0].transactionIds[index];
@@ -93,6 +104,8 @@ class Audit {
         lastOverflowRate = mempool[txid].effectiveFeePerVsize;
       } else if (Math.floor(mempool[txid].effectiveFeePerVsize * 100) <= (lastOverflowRate * 100)) { // tolerance of 0.01 sat/vb
         if (isCensored[txid]) {
+          sameFeeRate++;
+          sameFeeRateWeight += mempool[txid].weight;
           delete isCensored[txid];
         }
       }
@@ -100,8 +113,12 @@ class Audit {
       index--;
     }
 
+    logger.warn(`reduced estimate of ${Object.keys(isCensored).length} censored. ${sameFeeRate} txs / ${sameFeeRateWeight} weight by same fee band ${lastOverflowRate}s/vb`);
+
     const numCensored = Object.keys(isCensored).length;
     const score = matches.length > 0 ? (matches.length / (matches.length + numCensored)) : 0;
+
+    logger.warn(`score: ${matches.length} / (${matches.length} + ${numCensored}) = ${score}`);
 
     return {
       censored: Object.keys(isCensored),
